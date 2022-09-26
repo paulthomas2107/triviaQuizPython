b378823ac4c0b4d1b49e8f2888b2fe486e9f0d36
Paul Thomas <paulthomas2107@gmail.com>
2022-09-26 13:49:14 +0100

    More...

--- programmer.py
+++ programmer.py
@@ -8,8 +8,11 @@ widgets = {
     "logo":  [],
     "button": [],
     "score": [],
-    "question": []
-
+    "question": [],
+    "answer1": [],
+    "answer2": [],
+    "answer3": [],
+    "answer4": [],
 }
 
 app = QApplication(sys.argv)
@@ -22,20 +25,39 @@ window.setStyleSheet("background: #161219;")
 grid = QGridLayout()
 
 
-def frame1():
-    # Button widget
-    button = QPushButton("PLAY")
+def clear_widgets():
+    for widget in widgets:
+        if widgets[widget]:
+            widgets[widget][-1].hide()
+        for i in range(0, len(widgets[widget])):
+            widgets[widget].pop()
+
+
+def start_game():
+    clear_widgets()
+    frame2()
+
+
+def create_buttons(answer, l_margin, r_margin):
+    button = QPushButton(answer)
     button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setFixedWidth(485)
     button.setStyleSheet(
         "*{border: 4px solid '#BC006C';" +
-        "border-radius: 45px;" +
-        "font-family: 'Shanti';" +
-        "font-size: 35px;" +
+        "margin-left: " + str(l_margin) + "px;" +
+        "margin-right: " + str(r_margin) + "px;" +
         "color: 'white';" +
-        "padding: 25px 0;" +
-        "margin: 100px 200px;}" +
+        "font-family: 'Optima';" +
+        "font-size: 16px;" +
+        "border-radius: 25px;" +
+        "padding: 15px 0;" +
+        "margin-top: 20px;}" +
         "*:hover{background: '#BC006C';}"
     )
+    return button
+
+
+def frame1():
     # Show logo
     image = QPixmap("logo.png")
     logo = QLabel()
@@ -43,7 +65,22 @@ def frame1():
     logo.setAlignment(QtCore.Qt.AlignCenter)
     logo.setStyleSheet("margin-top: 100px")
 
+    # Button widget
+    button = QPushButton("PLAY")
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setStyleSheet(
+        "*{border: 4px solid '#BC006C';" +
+        "border-radius: 45px;" +
+        "font-family: 'Optima';" +
+        "font-size: 35px;" +
+        "color: 'white';" +
+        "padding: 25px 0;" +
+        "margin: 100px 200px;}" +
+        "*:hover{background: '#BC006C';}"
+    )
+
     widgets["logo"].append(logo)
+    button.clicked.connect(start_game)
     widgets["button"].append(button)
 
     grid.addWidget(widgets["logo"][-1], 0, 0)
@@ -55,11 +92,11 @@ def frame2():
     score.setAlignment(QtCore.Qt.AlignRight)
     score.setStyleSheet(
         '''
-        font-family: 'Shanti';
+        font-family: 'Optima';
         font-size: 35px;
         color: 'white';
         padding: 15px 10px;
-        margin: 20px 200px;
+        margin: 100px 200px;
         background: '#64A314';
         border: 1px solid '#64A314';
         border-radius: 35px;
@@ -72,7 +109,7 @@ def frame2():
     question.setWordWrap(True)
     question.setStyleSheet(
         '''
-             font-family: 'Shanti';
+             font-family: 'Optima';
              font-size: 25px;
              color: 'white';
              padding: 75px;
@@ -80,11 +117,33 @@ def frame2():
     )
     widgets["question"].append(question)
 
+    button1 = create_buttons("answer1", 85, 5)
+    button2 = create_buttons("answer2", 5, 85)
+    button3 = create_buttons("answer3", 85, 5)
+    button4 = create_buttons("answer4", 5, 85)
+
+    widgets["answer1"].append(button1)
+    widgets["answer2"].append(button2)
+    widgets["answer3"].append(button3)
+    widgets["answer4"].append(button4)
+
+    image = QPixmap("logo_bottom.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignCenter)
+    logo.setStyleSheet("margin-top: 75px; margin-bottom: 30px;")
+    widgets["logo"].append(logo)
+
     grid.addWidget(widgets["score"][-1], 0, 1)
     grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+    grid.addWidget(widgets["answer1"][-1], 2, 0)
+    grid.addWidget(widgets["answer2"][-1], 2, 1)
+    grid.addWidget(widgets["answer3"][-1], 3, 0)
+    grid.addWidget(widgets["answer4"][-1], 3, 1)
+    grid.addWidget(widgets["logo"][-1], 4, 0, 1, 2)
 
 
-frame2()
+frame1()
 
 window.setLayout(grid)
 window.show()
